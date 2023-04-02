207e98b8194f3644de8932a51d65166d04503e4f
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-02 19:33:54 +0530

    added the input feild for initial capital

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className=" w-full !h-full bg-neutral-200">
-       dfds
+      <label htmlFor='initialCap'>Initial Capital</label>
+       <input type="number" id="initialCap" className='bg-green-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500'></input>
     </div>
   );
 }
