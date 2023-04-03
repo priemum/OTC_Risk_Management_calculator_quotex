78f8f62e7d357f89c6a0aef4f96203ac36edb06c
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-04 01:48:08 +0530

    ui updates

--- src/components/Containers/TradesContainer/TradesContainer.jsx
+++ src/components/Containers/TradesContainer/TradesContainer.jsx
@@ -1,8 +1,14 @@
 import './TradesContainer.scss'
+import { motion } from "framer-motion";
 
 const TradesContainer=(props)=>{
     return(
-        <div className="app__tradesContainer">{props.children}</div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ type: "linear", stiffness: 400, damping: 100 }}
+          viewport={{ once: true }}
+         className="app__tradesContainer">{props.children}</motion.div>
     )
 }
 
--- src/components/UIelements/TradeItem/TradeItem.jsx
+++ src/components/UIelements/TradeItem/TradeItem.jsx
@@ -1,12 +1,24 @@
-import './TradeItem.scss';
+import "./TradeItem.scss";
+import { motion } from "framer-motion";
 
-const TradeItem = (props)=>{
-    return(
-        <div className={`app__tradeItem`}>
-           <div className="tradeItem__leftsec">dfdsfdsf</div>
-           <div className="tradeItem__rightsec">dsfdsf</div>
+const TradeItem = (props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 1.2 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+      className={`app__tradeItem`}
+    >
+      <div className="tradeItem__leftsec">
+        <div className="leftsec__amount">
+         <div className="title"> Trade Amount : </div><div className="title amount">{props.amount}</div>
         </div>
-    )
-}
+      </div>
+      <div className="tradeItem__rightsec">
+        <div className="title">Result</div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default TradeItem;
