befe250c7e3d162ba89bd86999e0ff06e54372fd
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-03 09:13:31 +0530

    installed react-google-font-loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import AnimatedBG from './components/Utility/AnimatedBG';
-import './App.scss';
+import AnimatedBG from "./components/Utility/AnimatedBG";
+import "./App.scss";
+import GoogleFontLoader from "react-google-font-loader";
 
 function App() {
   return (
     <>
-    <div className="bg"><AnimatedBG/></div>
-    <div className="__container ">
-      <div className="content"><label htmlFor='initialCap'>Initial Capital</label>
-       <input type="number" id="initialCap" className='' step="none"></input></div>
-      
-    </div></>
+      <GoogleFontLoader
+        fonts={[
+          {
+            font: "Roboto",
+            weights: [400, "400i"],
+          },
+          {
+            font: "Roboto Mono",
+            weights: [400, 700],
+          },
+        ]}
+        subsets={["cyrillic-ext", "greek"]}
+      />
+      <div className="bg">
+        <AnimatedBG />
+      </div>
+      <div className="__container ">
+        <div className="content">
+          <label style={{fontFamily:"Roboto Mono"}} htmlFor="initialCap">Initial Capital</label>
+          <input type="number" id="initialCap" className="" step="none"></input>
+        </div>
+      </div>
+    </>
   );
 }
 
